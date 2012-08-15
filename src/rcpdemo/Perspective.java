040c56d88b438fe8014212f9b930a555c427ff0f
yinyuwen <yuwen.yin@dragonflow.com>
2012-08-15 11:14:19 +0800

    The update three

--- src/rcpdemo/Perspective.java
+++ src/rcpdemo/Perspective.java
@@ -1,11 +1,18 @@
 package rcpdemo;
 
+import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
 public class Perspective implements IPerspectiveFactory {
 
 	public void createInitialLayout(IPageLayout layout) {
-	
+		layout.setEditorAreaVisible(true);
+		// layout.addStandaloneView(WorkSpaceHeader.ID, false, IPageLayout.TOP,
+		// 0.15f, layout.getEditorArea());
+		IFolderLayout navFold = layout.createFolder("Navigator",
+				IPageLayout.LEFT, 0.95f, layout.getEditorArea());
+		navFold.addView("rcpdemo.view1");
+		navFold.addView("rcpdemo.view2");
 	}
 }
--- src/rcpdemo/ViewPart2.java
+++ src/rcpdemo/ViewPart2.java
@@ -17,6 +17,7 @@ import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.wb.swt.SWTResourceManager;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.wb.swt.layout.grouplayout.GroupLayout;
 
 public class ViewPart2 extends ViewPart {
 	private Text text_1;
@@ -30,67 +31,76 @@ public class ViewPart2 extends ViewPart {
 	@Override
 	public void createPartControl(Composite parent) {
 		parent.setFont(SWTResourceManager.getFont("ËÎÌו", 16, SWT.NORMAL));
-		parent.setLayout(new FormLayout());
 		
-		Label label = new Label(parent, SWT.CENTER);
+		Label label = new Label(parent, SWT.HORIZONTAL | SWT.CENTER);
 		label.setFont(SWTResourceManager.getFont("ËÎÌו", 16, SWT.NORMAL));
-		FormData fd_label = new FormData();
-		fd_label.top = new FormAttachment(0, 40);
-		fd_label.left = new FormAttachment(0, 208);
-		fd_label.right = new FormAttachment(100, -229);
-		fd_label.bottom = new FormAttachment(0, 88);
-		label.setLayoutData(fd_label);
 		label.setText("\u7528\u6237\u6CE8\u518C");
 		
 		Label label_1 = new Label(parent, SWT.NONE);
-		FormData fd_label_1 = new FormData();
-		fd_label_1.top = new FormAttachment(label, 7);
-		fd_label_1.left = new FormAttachment(0, 191);
-		label_1.setLayoutData(fd_label_1);
 		label_1.setText("\u7528\u6237\u540D");
 		
 		Label label_2 = new Label(parent, SWT.NONE);
-		FormData fd_label_2 = new FormData();
-		fd_label_2.top = new FormAttachment(label_1, 17);
-		fd_label_2.right = new FormAttachment(label_1, 0, SWT.RIGHT);
-		label_2.setLayoutData(fd_label_2);
 		label_2.setText("\u5BC6\u7801");
 		
 		Label label_3 = new Label(parent, SWT.NONE);
-		FormData fd_label_3 = new FormData();
-		fd_label_3.top = new FormAttachment(label_2, 17);
-		fd_label_3.right = new FormAttachment(label_1, 0, SWT.RIGHT);
-		label_3.setLayoutData(fd_label_3);
 		label_3.setText("\u91CD\u590D\u5BC6\u7801");
 		
 		text_1 = new Text(parent, SWT.BORDER);
-		FormData fd_text_1 = new FormData();
-		fd_text_1.top = new FormAttachment(label, 36);
-		fd_text_1.right = new FormAttachment(label_2, 148, SWT.RIGHT);
-		fd_text_1.left = new FormAttachment(label_2, 27);
-		text_1.setLayoutData(fd_text_1);
 		
 		Button button_1 = new Button(parent, SWT.NONE);
-		FormData fd_button_1 = new FormData();
-		fd_button_1.top = new FormAttachment(label_3, 34);
-		fd_button_1.left = new FormAttachment(0, 221);
-		fd_button_1.right = new FormAttachment(100, -278);
-		button_1.setLayoutData(fd_button_1);
 		button_1.setText("\u6CE8\u518C");
 		
 		text = new Text(parent, SWT.BORDER);
-		FormData fd_text = new FormData();
-		fd_text.right = new FormAttachment(text_1, 0, SWT.RIGHT);
-		fd_text.top = new FormAttachment(label, 6);
-		fd_text.left = new FormAttachment(label_1, 27);
-		text.setLayoutData(fd_text);
 		
 		text_2 = new Text(parent, SWT.BORDER);
-		FormData fd_text_2 = new FormData();
-		fd_text_2.right = new FormAttachment(label_3, 148, SWT.RIGHT);
-		fd_text_2.top = new FormAttachment(text_1, 11);
-		fd_text_2.left = new FormAttachment(label_3, 27);
-		text_2.setLayoutData(fd_text_2);
+		GroupLayout gl_parent = new GroupLayout(parent);
+		gl_parent.setHorizontalGroup(
+			gl_parent.createParallelGroup(GroupLayout.LEADING)
+				.add(gl_parent.createSequentialGroup()
+					.add(204)
+					.add(label, GroupLayout.PREFERRED_SIZE, 157, GroupLayout.PREFERRED_SIZE))
+				.add(gl_parent.createSequentialGroup()
+					.add(191)
+					.add(label_1)
+					.add(27)
+					.add(text, GroupLayout.PREFERRED_SIZE, 121, GroupLayout.PREFERRED_SIZE))
+				.add(gl_parent.createSequentialGroup()
+					.add(203)
+					.add(label_2)
+					.add(27)
+					.add(text_1, GroupLayout.PREFERRED_SIZE, 121, GroupLayout.PREFERRED_SIZE))
+				.add(gl_parent.createSequentialGroup()
+					.add(179)
+					.add(label_3)
+					.add(27)
+					.add(text_2, GroupLayout.PREFERRED_SIZE, 121, GroupLayout.PREFERRED_SIZE))
+				.add(gl_parent.createSequentialGroup()
+					.add(221)
+					.add(button_1, GroupLayout.PREFERRED_SIZE, 95, GroupLayout.PREFERRED_SIZE))
+		);
+		gl_parent.setVerticalGroup(
+			gl_parent.createParallelGroup(GroupLayout.LEADING)
+				.add(gl_parent.createSequentialGroup()
+					.add(41)
+					.add(label, GroupLayout.PREFERRED_SIZE, 48, GroupLayout.PREFERRED_SIZE)
+					.add(5)
+					.add(gl_parent.createParallelGroup(GroupLayout.LEADING)
+						.add(gl_parent.createSequentialGroup()
+							.add(1)
+							.add(label_1))
+						.add(text, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.add(12)
+					.add(gl_parent.createParallelGroup(GroupLayout.LEADING)
+						.add(label_2)
+						.add(text_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.add(11)
+					.add(gl_parent.createParallelGroup(GroupLayout.LEADING)
+						.add(label_3)
+						.add(text_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.add(28)
+					.add(button_1))
+		);
+		parent.setLayout(gl_parent);
 	}
 
 	@Override
